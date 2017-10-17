4f58fe62c5c4a413740f268612df85bd2a9a2ae0
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-10-17 17:22:29 -0200

    M  src/Core.php

--- src/Core.php
+++ src/Core.php
@@ -1,4 +1,8 @@
 <?php
+/**
+* Basic
+* Micro framework em PHP
+*/
 namespace Basic;
 
 use Basic\Auth;
@@ -11,149 +15,419 @@ use Basic\Sheet;
 use Basic\Upload;
 use Basic\View;
 
-class Core{
-    var $db_cfg;
-    var $view;
-    function __construct($cfg=null){
-        $this->db_cfg=@$cfg['mysql'];
-        $this->mail_cfg=@$cfg['smtp'];
+/**
+* Classe core
+*/
+class Core
+{
+    public $dbCfg;
+    public $mailCfg;
+    public $view;
+    /**
+    * Chamada do sistema
+    * @param array $cfg
+    */
+    public function __construct($cfg=null)
+    {
+        $this->dbCfg=@$cfg['mysql'];
+        $this->mailCfg=@$cfg['smtp'];
         $this->view=new View();
     }
-    function app($name){
-        $filename=ROOT.'app/'.$name.'.php';
-        if(file_exists($filename)){
+    /**
+    * Retorna uma instância de uma classe App ou uma mensagem de erro
+    * @param  string $className
+    * @return mixed
+    */
+    public function app($className)
+    {
+        $filename=ROOT.'app/'.$className.'.php';
+        if (file_exists($filename)) {
             require $filename;
-            $class='App\\'.$name;
+            $class='App\\'.$className;
             return new $class($this);
-        }else{
+        } else {
             die('app <b>'.$filename.'</b> not found');
         }
     }
-    function array_to_sheet($array,$sheet_name){
+    /**
+    * Converte um array para uma planilha .csv, .ods ou . xlsx
+    * @param  array  $array
+    * @param  string $sheetName
+    * @return bool
+    */
+    public function arrayToSheet($sheetData, $sheetName)
+    {
         $obj=new Sheet();
-        return $obj->to_sheet($array,$sheet_name);
+        return $obj->toSheet($sheetData, $sheetName);
     }
-    function auth(){
-        return new Auth($this->db_cfg);
+    /**
+    * Classe Auth
+    * @return mixed
+    */
+    private function auth()
+    {
+        return new Auth($this->dbCfg);
     }
-    function count($table,$where){
-        return $this->db()->count($table,$where);
+    /**
+    * Conta quantas linhas atendem ao WHERE
+    * @param  string  $tableName
+    * @param  array   $where
+    * @return integer
+    */
+    public function count($tableName, $where)
+    {
+        return $this->db()->count($tableName, $where);
     }
-    function create($table,$data){
-        return $this->db()->create($table,$data);
+    /**
+    * Adiciona uma linha na tabela
+    * @param  string $tableName
+    * @param  array  $data
+    * @return mixed
+    */
+    public function create($tableName, $data)
+    {
+        return $this->db()->create($tableName, $data);
     }
-    function db(){
-        return new DB($this->db_cfg);
+    /**
+    * Classe DB
+    * @return object
+    */
+    private function db()
+    {
+        return new DB($this->dbCfg);
     }
-    function delete($table, $where){
-        return $this->db()->delete($table, $where);
+    /**
+    * Apagar linhas na tabela
+    * @param  string
+    * @param  array
+    * @return bool
+    */
+    public function delete($tableName, $where)
+    {
+        return $this->db()->delete($tableName, $where);
     }
-    function drop_all(){
-        return $this->migration()->drop_all();
+    /**
+    * Apaga todas as tabelas do banco de dados
+    * @return bool
+    */
+    public function dropAll()
+    {
+        return $this->migration()->dropAll();
     }
-    function first_word($str){
-        return $this->view->first_word($str);
+    /**
+    * Retorna a primeira palavra de uma frase
+    * @param  string $phrase
+    * @return string
+    */
+    public function firstWord($phrase)
+    {
+        return $this->view->firstWord($phrase);
     }
-    function get($url,$agent,$cookie){
+    /**
+    * Baixa um arquivo da internet através do método GET
+    * @param  string $url
+    * @param  string $agent
+    * @param  array  $cookie
+    * @return mixed
+    */
+    public function get($url, $agent, $cookie)
+    {
         $obj=new Download();
-        return $obj->get($url,$agent,$cookie);
+        return $obj->get($url, $agent, $cookie);
     }
-    function i18n($key,$print=true){
-        return $this->view->i18n($key,$print);
+    /**
+    * Sistema básico de internacionalização através do arquivo /view/i18n.php
+    * @param  string  $key
+    * @param  boolean $print
+    * @return string
+    */
+    public function i18n($key, $print=true)
+    {
+        return $this->view->i18n($key, $print);
     }
-    function image_auto_orient($src,$dst=false){
-        return $this->image()->auto_orient($src,$dst);
+    /**
+    * Corrige a orientação da imagem automáticamente
+    * @param  string  $src
+    * @param  mixed $dst
+    * @return bool
+    */
+    public function imageAutoOrient($src, $dstFile=false)
+    {
+        return $this->image()->autoOrient($src, $dstFile);
     }
-    function image_crop($src,$dst,$x1,$y1,$x2,$y2){
-        return $this->image($src);
+    /**
+    * Recorta uma imagem nas coordenadas especificadas
+    * @param  string $src
+    * @param  string  $dstFile
+    * @param  integer $x1
+    * @param  integer $y1
+    * @param  integer $x2
+    * @param  integer $y2
+    * @return bool
+    */
+    public function imageCrop($src, $dstFile, $x1, $y1, $x2, $y2)
+    {
+        return $this->image()->crop($src, $dstFile, $x1, $y1, $x2, $y2);
     }
-    function image(){
+    /**
+    * Classe Image
+    * @return object
+    */
+    private function image()
+    {
         return new Image();
     }
-    function image_info($src){
+    /**
+    * Retorna informações sobre uma imagem
+    * @param  string $src
+    * @return mixed
+    */
+    public function imageInfo($src)
+    {
         return $this->image()->info($src);
     }
-    function image_resize($src,$dst,$max_width,$max_height){
-        return $this->image()->resize($src,$dst,$max_width,$max_height);
+    /**
+    * Redimensiona uma imagem
+    * @param  string $src
+    * @param  string $dstFile
+    * @param  integer $maxWidth
+    * @param  integer $maxHeight
+    * @return bool
+    */
+    public function imageResize($src, $dstFile, $maxWidth, $maxHeight)
+    {
+        return $this->image()->resize($src, $dstFile, $maxWidth, $maxHeight);
     }
-    function image_thumb($src,$dst,$width,$height){
-        return $this->image()->thumb($src,$dst,$width,$height);
+    /**
+    * Cria uma miniatura da imagem
+    * @param  string  $srr
+    * @param  string  $dst
+    * @param  integer $width
+    * @param  integer $height
+    * @return bool
+    */
+    public function imageThumb($srr='', $dst='', $width=1, $height=1)
+    {
+        return $this->image()->thumb($src, $dst, $width, $height);
     }
-    function is_ajax(){
-        return $this->view->is_ajax();
+    /**
+    * Retorna true se a conexão for via Ajax e false se não for
+    * @return boolean
+    */
+    public function isAjax()
+    {
+        return $this->view->isAjax();
     }
-    function is_auth(){
-        return $this->auth()->is_auth();
+    /**
+    * Verifica se o usuário está autenticado e retorna os dados dele caso esteja
+    * @return boolean
+    */
+    public function isAuth()
+    {
+        return $this->auth()->isAuth();
     }
-    function json($data){
+    /**
+    * Converte os dados para JSON com header
+    * @param  mixed  $data
+    * @return string
+    */
+    public function json($data)
+    {
         return $this->view->json($data);
     }
-    function logout(){
+    /**
+    * Faz o logout do usuaŕio
+    * @return [type]
+    */
+    public function logout()
+    {
         return $this->auth()->logout();
     }
-    function mail(){
-        return new Mail($this->mail_cfg);
+    /**
+    * Classe Mail
+    * @return object
+    */
+    private function mail()
+    {
+        return new Mail($this->mailCfg);
     }
-    function method(){
+    /**
+    * Retorna o método da requisição
+    * @return string
+    */
+    public function method()
+    {
         return @$_SERVER['REQUEST_METHOD'];
     }
-    function migrate_all(){
-        return $this->migration()->migrate_all();
+    /**
+    * Migra todas as tabelas plain text de /table
+    * @return string
+    */
+    public function migrateAll()
+    {
+        return $this->migration()->migrateAll();
     }
-    function migration(){
-        return new Migration($this->db_cfg);
+    /**
+    * Classe Migration
+    * @return string
+    */
+    private function migration()
+    {
+        return new Migration($this->dbCfg);
     }
-    function post($url,$params,$agent,$cookie){
+    /**
+    * Baixa um arquivo de internet através do método POST
+    * @param  string $url
+    * @param  array  $params
+    * @param  string $agent
+    * @param  array  $cookie
+    * @return mixed
+    */
+    public function post($url='', $params=[], $agent='', $cookie=[])
+    {
         $obj=new Download();
-        return $obj->post($url,$params,$agent,$cookie);
+        return $obj->post($url, $params, $agent, $cookie);
     }
-    function query($sql){
+    /**
+    * Requisição SQL
+    * @param  string $sql
+    * @return mixed
+    */
+    public function query(string $sql)
+    {
         return $this->db()->query($sql);
     }
-    function read($table,$where){
-        return $this->db()->read($table,$where);
+    /**
+    * Lê uma linha de uma tabela
+    * @param  string $tableName
+    * @param  array  $where
+    * @return [type]
+    */
+    public function read(string $tableName, array $where)
+    {
+        return $this->db()->read($tableName, $where);
     }
-    function redirect($url){
+    /**
+    * Redireciona para outra URL
+    * @param  string $url
+    * @return mixed
+    */
+    public function redirect(string $url)
+    {
         header("Location: ".$url);
     }
-    function segment($key = null){
+    /**
+    * Retorna os segmentos da URL
+    * @param  integer $key
+    * @return mixed
+    */
+    public function segment($key = null)
+    {
         return $this->view->segment($key);
     }
-    function select($table,$where=null){
-        return $this->db()->select($table,$where);
+    /**
+    * Faz uma requisição SELECT na tabela especificada
+    * @param  string $tableName
+    * @param  mixed $where
+    * @return mixed
+    */
+    public function select(string $tableName, array $where=null)
+    {
+        return $this->db()->select($tableName, $where);
     }
-    function send($to,$subject,$html,$plain=false){
-        return $this->mail()->send($to,$subject,$html,$plain);
+    /**
+    * Envia uma mensagem de email
+    * @param  string  $toAddress
+    * @param  string  $subject
+    * @param  string  $html
+    * @param  mixed $plain
+    * @return [type]
+    */
+    public function send(string $toAddress, string $subject, string $html, $plain=false)
+    {
+        return $this->mail()->send($toAddress, $subject, $html, $plain);
     }
-    function sheet_to_array($sheet_name){
+    /**
+    * Converte uma tabela .ods, .csv ou .xlsx para um array
+    * @param  string $sheetName
+    * @return mixed
+    */
+    public function sheetToArray(string $sheetName)
+    {
         $obj=new Sheet();
-        return $obj->to_array($sheet_name);
+        return $obj->toArray($sheetName);
     }
-    function signin(){
+    /**
+    * Autentica o usuário baseado nas variáveis $_POST
+    * @return mixed
+    */
+    public function signin()
+    {
         return $this->auth()->signin();
     }
-    function signup($user=false){
+    /**
+    * Cadastra de usuário baseado nas variáveis $_POST e no parâmetro $user
+    * @param  array $user
+    * @return mixed
+    */
+    public function signup($user=false)
+    {
         return $this->auth()->signup($user);
     }
-    function slug($text,$set=true){
-        if($set){
+    /**
+    * Adiciona ou remove underlines de uma string
+    * @param  string  $text
+    * @param  boolean $set
+    * @return string
+    */
+    public function slug(string $text, $set=true)
+    {
+        if ($set) {
             return str_replace(' ', '_', $text);
-        }else{
+        } else {
             return str_replace('_', ' ', $text);
         }
     }
-    function trucate_all(){
-        return $this->migration()->truncate_all();
+    /**
+    * Apaga todos os dados de todas as tabelas
+    * @return mixed
+    */
+    public function trucateAll()
+    {
+        return $this->migration()->truncateAll();
     }
-    function update($table, $data, $where){
-        return $this->db()->update($table, $data, $where);
+    /**
+    * Atualiza dados na tabela
+    * @param  string $tableName
+    * @param  array  $data
+    * @param  array  $where
+    * @return bool
+    */
+    public function update(string $tableName, array $data, array $where)
+    {
+        return $this->db()->update($tableName, $data, $where);
     }
-    function upload($name,$exts){
+    /**
+    * Processa o upload de um arquivo
+    * @param  string $name
+    * @param  array  $exts
+    * @return array
+    */
+    public function upload(string $name, array $exts)
+    {
         $Upload=new Upload();
-        return $Upload->upload($name,$exts);
+        return $Upload->upload($name, $exts);
     }
-    function view($name,$data=null,$print=true){
+    /**
+    * Retorna uma /view
+    * @param  string  $name
+    * @param  mixed  $data
+    * @param  boolean $print
+    * @return string
+    */
+    public function view(string $name, $data=null, $print=true)
+    {
         $data['b']=$this;
-        return $this->view->view($name,$data,$print);
+        return $this->view->view($name, $data, $print);
     }
 }
